46cdf316094dff750c609910875ffacd50bd1d8a
Salman Khan <greate43.sk@gmail.com>
2020-04-11 21:15:18 +1000

    upding gradle

--- lib/src/main/java/com/github/omadahealth/typefaceview/TypefaceEditText.java
+++ lib/src/main/java/com/github/omadahealth/typefaceview/TypefaceEditText.java
@@ -3,7 +3,7 @@ package com.github.omadahealth.typefaceview;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.databinding.BindingAdapter;
+import androidx.databinding.BindingAdapter;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.util.AttributeSet;
